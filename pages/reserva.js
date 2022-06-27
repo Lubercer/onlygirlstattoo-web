55d0ed52252ac691a724e17dc6f7564f27bbafb7
Luna <luna_bercernelo@icloud.com>
2022-06-27 20:47:27 -0300

    se agrego desafio

--- pages/reserva.js
+++ pages/reserva.js
@@ -11,12 +11,22 @@ const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
 const envio = x => + 750;
 
-let descuento = microdermal*0.10 //10% de descuento
-
-//                          
-
-function Preciototal() {
+let descuento = microdermal*0.10 //10% de descuento//  
+ 
+function precioTotal() {
     let preciopiercing = resta(suma(microdermal, envio(microdermal)), descuento);  
     console.log(preciopiercing);
+    alert ("Bienvenidx a Only Girls Tattoo");
+let entrada = prompt ("Seleccione 1 para saber el valor de nuestro microdermal") 
+let aceptar = confirm ("¿Desea saber el precio con envio?")
+if (aceptar == true) {
+    alert ("Microdermal de Titanio (10% de descuento) + envio: "+ preciopiercing);}
+    else { alert ("Retirando por nuestro local + 10% de descuento: $1800")
+
+    }
 }
-Preciototal();
+precioTotal();                  
+
+
+
+
