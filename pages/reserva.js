f069d21624bf2503cb9bf9798a32706506ad5125
Luna <luna_bercernelo@icloud.com>
2022-06-29 22:31:47 -0300

    Desafio Arrays

--- pages/reserva.js
+++ pages/reserva.js
@@ -1,31 +1,24 @@
 
 /* productos */
-let microdermal = 2000
-let daith = 900
-let industrial = 850
-let tragus = 800
-let labret = 750
+let productos =[
+ {nombre: "microdermal", precio: 2000},
+{nombre:"daith", precio: 900},
+{nombre: "industrial",precio:850},
+{nombre: "tragus", precio:800},
+{nombre:"labret", precio:750},];
 
-/* Calcular productos + envio - 10% de descuento */
-const suma = (a, b) => a + b;
-const resta = (a, b) => a - b;
-const envio = x => + 750;
+let asignarFiltro = prompt("Ingrese filtro (nombre o precio)");
 
-let descuento = microdermal*0.10 //10% de descuento//  
- 
-function precioTotal() {
-    let preciopiercing = resta(suma(microdermal, envio(microdermal)), descuento);  
-    console.log(preciopiercing);
-    alert ("Bienvenidx a Only Girls Tattoo");
-let entrada = prompt ("Seleccione 1 para saber el valor de nuestro microdermal") 
-let aceptar = confirm ("¿Desea saber el precio con envio?")
-if (aceptar == true) {
-    alert ("Microdermal de Titanio (10% de descuento) + envio: "+ preciopiercing);}
-    else { alert ("Retirando por nuestro local + 10% de descuento: $1800")
-
-    }
+if (asignarFiltro === "nombre") {
+    let filtro = prompt("Ingrese el nombre que corresponde a la perforacion");
+    let resultados = productos.filter ((elemento) => elemento.nombre.includes(filtro));
+   console.log(resultados);
 }
-precioTotal();                  
+else if (asignarFiltro === "precio") {
+    let filtro = parseInt (prompt("Ingrese el precio que corresponde a la perforacion"))
+    let resultados = productos.filter ((elemento) => elemento.precio > filtro);
+    console.log(resultados);
+}
 
 
 
