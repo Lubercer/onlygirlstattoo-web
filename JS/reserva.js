a4371e1eed9b22aca06ee40fd51b87e7da1f7e42
Luna <luna_bercernelo@icloud.com>
2022-07-02 18:47:39 -0300

    Primer entrega

---
+++ JS/reserva.js
@@ -0,0 +1,59 @@
+
+class Reserva{
+    constructor(nombre,sucursal,fecha, precio){
+        this.nombre = nombre
+        this.sucursal = sucursal
+        this.fecha = fecha
+    }
+}
+const userReserva = []
+
+// arrays de los diferentes productos
+const piercingsArray = [
+  {nombre: "microdermal", price: 2000},
+  {nombre: "daith", price: 900},
+  {nombre:"industrial",price: 850},
+  {nombre: "tragus", price: 800},
+  {nombre: "labret", price: 750}
+]
+
+/* Usaremos esta funcion para saber quien es y que busca el cliente */
+function datos() { 
+    let nombreApellido = prompt ("Por favor ingrese su nombre y apellido");
+    alert ("¡bienvenidx! " + nombreApellido + " haz click en ACEPTAR para continuar");
+}
+datos();
+
+  // función para pedir datos y guardar la reserva en un objeto ↓
+    const pedirDatosReserva = (nombre) =>{
+            nombre = prompt('¿Que perforación te vas a realizar?').toLowerCase()
+        if(nombre === 'microdermal' || nombre === 'daith' || nombre === 'industrial' || nombre === 'tragus' || nombre === 'labret'){
+            let sucursal = prompt('Elegí una de nuestras sucursales: Nordelta - Pacheco')
+            let fecha = prompt('Elegí la fecha (ej 15/03/22)')
+            alert ("¡Turno agendado! Gracias <3")
+        }else alert('Por favor escriba una opción existente')
+    }
+    
+    pedirDatosReserva()
+    
+    console.log(userReserva)
+
+
+  
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    
+
+
