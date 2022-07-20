cb74fe2450c93de9a1efef94058c1c341e88d650
Luna <luna_bercernelo@icloud.com>
2022-07-20 17:23:34 -0300

    se agrego libreria, se corrigio css y se modifico codigo

--- JS/reserva.js
+++ JS/reserva.js
@@ -1,6 +1,6 @@
 
 class Reserva{
-    constructor(nombre,sucursal,fecha, precio){
+    constructor(nombre,sucursal,fecha){
         this.nombre = nombre
         this.sucursal = sucursal
         this.fecha = fecha
@@ -21,19 +21,34 @@ const productosGuardados = (clave, valor) => {localStorage.setItem(clave, valor)
 
 productosGuardados ("listaDePiercing", JSON.stringify(piercingsArray));
 
-function mostrarInputs() {
+/* Buscamos almacenar toda la informacion para generar un turno */
+const turnos = [];
+
+window.onload = cargarEventos;
+
+function cargarEventos() {
+  const validar =  document.getElementById("formulario");
+  validar.addEventListener ("submit", mostrarDatos);  
+ 
+}
+/* Funcion para generar nuevos turnos */
+function mostrarDatos(event) {
+   
+    const validar = event.target;
+    event.preventDefault();
     const nombreIngresado = document.getElementById ('nombreApellido').value;
-    const select = document.getElementById('perforacion').value;
+    const fecha = document.getElementById('fecha').value;
+    const select = document.getElementById("perforacion").value;
     const sucur = document.getElementById('sucursales').value;
     const horario = document.getElementById('horario').value;
-    console.log(nombreIngresado);
-    console.log(select);
-    console.log(sucur);
-    console.log(horario);
-     }
-
-     const datoaGuardados = (clave, valor) => {localStorage.setItem(clave, valor)};
-
-     datoaGuardados ("datosDeTurnos", (mostrarInputs()));
-     
-
+    const mostrarDatos = {nombre: nombreIngresado, fecha: fecha, perforacion: select, sucursal: sucur , horario: horario };
+    turnos.push (mostrarDatos);
+    Swal.fire(
+        '¡Turno agendado!',
+        'Nos vemos <3',
+        'success'
+      )
+    /* local Storage de turnos*/
+    const turnosGuardados = (clave, valor) => {localStorage.setItem(clave, valor)};
+    turnosGuardados ("listaDeTurnos", JSON.stringify(turnos));
+}
